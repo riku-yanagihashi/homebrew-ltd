eef8d1c69fa910400957b133f01d4a05d6e21fb2
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2024-06-27 02:35:11 +0900

    Update ltd formula with correct URL and SHA256

--- Formula/ltd.rb
+++ Formula/ltd.rb
@@ -2,7 +2,7 @@ class Ltd < Formula
   desc "A simple and efficient CLI todo application"
   homepage "https://github.com/riku-yanagihashi/Lazy-todo"
   url "https://github.com/riku-yanagihashi/Lazy-todo/releases/download/v0.1.0/ltd-v0.1.0.tar.gz"
-  sha256 "edca889a1e3df6e2e478d5a33f66a4fb35b0c1590e52f9092b829c7bb60a59f5  ltd-v0.1.0.tar.gz"
+  sha256 "edca889a1e3df6e2e478d5a33f66a4fb35b0c1590e52f9092b829c7bb60a59f5"
   license "MIT"
 
   def install
