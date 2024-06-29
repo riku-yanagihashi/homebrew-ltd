37bc93b9177752ef8729996b1d5b084af982f62c
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2024-06-29 13:19:04 +0900

    push!push!push!

--- Formula/ltd.rb
+++ Formula/ltd.rb
@@ -2,7 +2,7 @@ class Ltd < Formula
   desc "A simple and efficient CLI todo application"
   homepage "https://github.com/riku-yanagihashi/Lazy-todo"
   url "https://github.com/riku-yanagihashi/Lazy-todo/releases/download/1.0.0/ltd-v1.0.0.tar.gz"
-  sha256 "339d3dd21ed61b70f27a36e516b05a0bdf8451630c973b4c6bdce91343d0cdd8"
+  sha256 "759b9f23076a6ff929529b7894c61301e8658a0cd75af427f1d5de7f446efc3c"
   license "MIT"
 
   def install
